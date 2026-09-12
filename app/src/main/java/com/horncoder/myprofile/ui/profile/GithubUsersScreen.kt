102dcad67e15e464783408dc02e877fb620609a1
samir mukhtar <samirsamawade@gmail.com>
2026-09-12 09:41:38 +0300

    userDetail compose

--- app/src/main/java/com/horncoder/myprofile/ui/profile/GithubUsersScreen.kt
+++ app/src/main/java/com/horncoder/myprofile/ui/profile/GithubUsersScreen.kt
@@ -2,11 +2,18 @@ package com.horncoder.myprofile.ui.profile
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,6 +22,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.horncoder.myprofile.model.response.GithubUser
 
 @Composable
 fun GithubUsersScreen(viewModel: GithubUsersViewModel = viewModel()) {
@@ -41,26 +50,47 @@ fun GithubUsersScreen(viewModel: GithubUsersViewModel = viewModel()) {
         }
 
         is GithubUiState.Success -> {
-            LazyColumn {
-                items(state.data){user ->
-                    Text(text = user.login)
-                }
-            }
-//            Text(text = "j")
+            UserList(state.data)
         }
 
     }
 }
 
-//@Composable
-//fun UserDetail(data: GithubUser) {
-//    LazyColumn(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(8.dp)
-//    ) {
-//        items(5) {
-//            Text(text = "1")
-//        }
-//    }
-//}
+@Composable
+fun UserList(data: List<GithubUser>) {
+    LazyColumn {
+        items(data) { user ->
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                elevation = CardDefaults.cardElevation(2.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+
+            ){
+
+                Row {
+                    AsyncImage(
+                        model = user.avatarUrl,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .size(88.dp)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(16.dp)
+
+                    ) {
+                        Text(
+                            text = user.login,
+                            style = MaterialTheme.typography.titleSmall
+
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
